2b199eb118799ee31574e7dae38535fa9e239faf
Jemuel <jem123maglupay@gmail.com>
2023-11-11 16:18:59 +0800

    for prod

--- dist/spa/assets/axios.11eeb3db.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.6cd80509.js";import{a}from"./axios.66c02ff4.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ dist/spa/assets/axios.64d9f1cd.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.fc4e3e87.js";import{a}from"./axios.66c02ff4.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
