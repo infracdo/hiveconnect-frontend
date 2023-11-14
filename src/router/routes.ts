339d6fd9d4f13b8bd46a7efd72bc5a465e9793a1
Jemuel <jem123maglupay@gmail.com>
2023-11-14 10:47:38 +0800

    added a route redirect for mainlayout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,7 @@ import { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
+    redirect: "/inet-config",
     component: () => import("layouts/MainLayout.vue"),
     children: [
       // { path: '', component: () => import('pages/IndexPage.vue') },
