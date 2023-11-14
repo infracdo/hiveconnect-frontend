0c4e66646e649e5f27438d5eb2aaf4940b13b635
Jemuel <jem123maglupay@gmail.com>
2023-11-14 17:10:37 +0800

    changed some css

--- src/App.vue
+++ src/App.vue
@@ -17,63 +17,63 @@ watch(
 </script>
 
 <style lang="sass">
-.body--dark
-  .my-sticky-last-column-table
-    thead tr:last-child th:last-child
-      /* bg color is important for th; just specify one */
-      background-color: #1D1D1D
-    td:last-child
-      background-color: #1D1D1D
-    th:last-child,
-    td:last-child
-      border-left: 1px solid rgb(255,255,255, 0.5)
+// .body--dark
+//   .my-sticky-last-column-table
+//     thead tr:last-child th:last-child
+//       /* bg color is important for th; just specify one */
+//       background-color: #1D1D1D
+//     td:last-child
+//       background-color: #1D1D1D
+//     th:last-child,
+//     td:last-child
+//       border-left: 1px solid rgb(255,255,255, 0.5)
 
-  @media (min-width: 1200px)
-    .my-sticky-last-column-table
-      thead tr:last-child th:last-child
-        background-color: none
-      td:last-child
-        background-color: none
+//   @media (min-width: 1200px)
+//     .my-sticky-last-column-table
+//       thead tr:last-child th:last-child
+//         background-color: none
+//       td:last-child
+//         background-color: none
 
-.my-sticky-last-column-table
-  thead tr:last-child th:last-child
+// .my-sticky-last-column-table
+//   thead tr:last-child th:last-child
 
-    background-color: white
-  td:last-child
-    background-color: white
+//     background-color: white
+//   td:last-child
+//     background-color: white
 
-  th:last-child,
-  td:last-child
-    border-left: 1.5px solid rgb(000,000,000, 0.5 )
-    position: sticky
-    right: 0
-    z-index: 1
+//   th:last-child,
+//   td:last-child
+//     border-left: 1.5px solid rgb(000,000,000, 0.5 )
+//     position: sticky
+//     right: 0
+//     z-index: 1
 
 
 
-.table-top-right
-  display: flex
-  flex-direction: column
-  gap: 0.5em
+// .table-top-right
+//   display: flex
+//   flex-direction: column
+//   gap: 0.5em
 
-@media (min-width: 480px)
-  .table-top-right
-    flex-direction: row
-    gap: 1em
+// @media (min-width: 480px)
+//   .table-top-right
+//     flex-direction: row
+//     gap: 1em
 
-@media (min-width: 1200px)
-  .my-sticky-last-column-table
-    thead tr:last-child th:last-child
+// @media (min-width: 1200px)
+//   .my-sticky-last-column-table
+//     thead tr:last-child th:last-child
 
-      background-color: white
+//       background-color: white
 
-    td:last-child
-      background-color: white
+//     td:last-child
+//       background-color: white
 
-    th:last-child,
-    td:last-child
-      border-left: none
-      position: relative
-      right: 0
-      z-index: 0
+//     th:last-child,
+//     td:last-child
+//       border-left: none
+//       position: relative
+//       right: 0
+//       z-index: 0
 </style>
