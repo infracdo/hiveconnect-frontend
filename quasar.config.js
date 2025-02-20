92a503a5c7b4574f1495f127c567bac62060cebb
infracdo <infracdo@gmail.com>
2025-02-20 11:08:30 +0800

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (ctx) {
       // analyze: true,
       // env: require('dotenv').config().parsed,
       env: {
-        PROVISION_API_URL: 'http://192.168.32.18:7550',
+        PROVISION_API_URL: 'http://192.168.32.21:7549',
         // PROVISION_API_URL: ctx.dev
         //   ? 'http://localhost:8080'
         //   : 'http://192.168.90.72:8080',
