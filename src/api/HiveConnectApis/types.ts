e092c5af3a5c2f1f2c6021b0325ae10646ba8842
infracdo <infracdo@gmail.com>
2025-02-25 14:33:53 +0800

    added log for debugging jwt

--- src/api/HiveConnectApis/types.ts
+++ src/api/HiveConnectApis/types.ts
@@ -36,6 +36,25 @@ export interface IClient {
   ssidName: string;
 }
 
+export interface IHiveClient {
+  id: number;
+  subscriberAccountNumber: string;
+  clientName: string; //added 10-22-24
+  ipAssigned: string;
+  onuSerialNumber: string;
+  site: string;
+  oltIp: string;
+  oltInterface: string;
+  onuMacAddress: string;
+  onuDeviceName: string;
+  packageType: string;
+  oltReportedUpstream: number;
+  oltReportedDownstream: number;
+  provision: string;
+  status: string;
+  ssidName: string;
+}
+
 export interface IMigrationSubscriber {
   id: number;
   subscriberAccountNumber: string;
